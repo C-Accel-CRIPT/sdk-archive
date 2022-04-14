e51c5c4e4e4b4531b134d7fb3b754c73ebf0a57a
Vinay Hegde <vhegde@citrine.io>
2022-04-14 02:34:49 -0700

    fix typo in setup; rm unused dependencies

--- setup.py
+++ setup.py
@@ -4,7 +4,9 @@ from setuptools import find_packages
 
 
 here = pathlib.Path(__file__).parent.resolve()
+# single-source of version in the package
 version = (here / "src" / "cript" / "VERSION.txt").read_text(encoding="utf-8")
+# TODO: fill out README with installation guide, smallest working example, etc.
 long_description = (here / "README.md").read_text(encoding="utf-8")
 
 
@@ -12,13 +14,18 @@ setup(
     name="cript",
     version=version,
     description="CRIPT REST API and data model bindings",
-    url="https://github.com/C-Accel-CRIPT/cript"
+    url="https://github.com/C-Accel-CRIPT/cript",
     author="CRIPT Development Team",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     include_package_data=True,
     python_requires=">=3.9",
-    install_requires=["beartype >= 0.10.0", "pint >= 0.18", "boto3 >= 1.21.10", "botocore >= 1.24.10", "globus-sdk >= 3.6.0"],
+    install_requires=[
+        "beartype >= 0.10.0",
+        "globus-sdk >= 3.6.0",
+        "pint >= 0.18",
+        "requests >= 2.27",
+    ],
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Topic :: Scientific/Engineering",
