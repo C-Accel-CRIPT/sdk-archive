caa0b28e1eca1a7d2d5ef4550b7afa70078e37d7
Vinay Hegde <vhegde@citrine.io>
2022-04-14 11:23:03 -0700

    move `NODE_CLASSES` back into top-level
    note that pint has to be instantiated before the node-level imports; otherwise
    will create circular import dependencies

--- src/cript/__init__.py
+++ src/cript/__init__.py
@@ -2,6 +2,53 @@ import pkg_resources
 import pint
 
 
+# Instantiate the Pint unit registry
+# https://pint.readthedocs.io/en/stable/tutorial.html#using-pint-in-your-projects
+# Note that this needs to be before the node imports below
+pint_ureg = pint.UnitRegistry(autoconvert_offset_to_baseunit=True)
+
+
+from cript.nodes import (  # noqa 402
+    User,
+    Group,
+    Reference,
+    Citation,
+    Collection,
+    File,
+    Data,
+    Condition,
+    Property,
+    Identifier,
+    Component,
+    Material,
+    Inventory,
+    Quantity,
+    Ingredient,
+    Process,
+    Experiment,
+)
+
+NODE_CLASSES = [
+    User,
+    Group,
+    Reference,
+    Citation,
+    Collection,
+    File,
+    Data,
+    Condition,
+    Property,
+    Identifier,
+    Component,
+    Material,
+    Inventory,
+    Quantity,
+    Ingredient,
+    Process,
+    Experiment,
+]
+
+
 # single-sourcing the package version
 version_file = pkg_resources.resource_filename("cript", "VERSION.txt")
 with open(version_file, "r") as fr:
@@ -9,8 +56,3 @@ with open(version_file, "r") as fr:
 
 VERSION = __version__
 __short_version__ = __version__.rpartition(".")[0]
-
-
-# Instantiate the Pint unit registry
-# https://pint.readthedocs.io/en/stable/tutorial.html#using-pint-in-your-projects
-pint_ureg = pint.UnitRegistry(autoconvert_offset_to_baseunit=True)
