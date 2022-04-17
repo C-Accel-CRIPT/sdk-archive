766dbfc1385cd42b12aeb5d6c17d7d9f79c2068f
Reid Mello <30907815+rjmello@users.noreply.github.com>
2022-04-17 16:40:34 -0400

    Move version definition to beginning

--- src/cript/__init__.py
+++ src/cript/__init__.py
@@ -2,6 +2,15 @@ import pkg_resources
 import pint
 
 
+# Single-sourcing the package version
+version_file = pkg_resources.resource_filename("cript", "VERSION.txt")
+with open(version_file, "r") as fr:
+    __version__ = fr.read().strip()
+
+VERSION = __version__
+__short_version__ = __version__.rpartition(".")[0]
+
+
 # Instantiate the Pint unit registry
 # https://pint.readthedocs.io/en/stable/tutorial.html#using-pint-in-your-projects
 # Note that this needs to be before the node imports below
@@ -49,12 +58,3 @@ NODE_CLASSES = [
 ]
 
 from cript.session import API  # noqa 401 402
-
-
-# single-sourcing the package version
-version_file = pkg_resources.resource_filename("cript", "VERSION.txt")
-with open(version_file, "r") as fr:
-    __version__ = fr.read().strip()
-
-VERSION = __version__
-__short_version__ = __version__.rpartition(".")[0]
