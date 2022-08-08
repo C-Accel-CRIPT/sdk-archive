8a14fa83e3b1d4c05ffe852d1d850e03064b11ba
Reid Mello <30907815+rjmello@users.noreply.github.com>
2022-08-08 11:01:50 -0400

    Add citations attribute to Equipment class

--- src/cript/nodes/secondary/equipment.py
+++ src/cript/nodes/secondary/equipment.py
@@ -5,6 +5,7 @@ from beartype import beartype
 
 from cript.nodes.secondary.base_secondary import BaseSecondary
 from cript.nodes.secondary.condition import Condition
+from cript.nodes.secondary.citation import Citation
 from cript.validators import validate_required, validate_key
 
 
@@ -26,11 +27,13 @@ class Equipment(BaseSecondary):
         key: str,
         description: Union[str, None] = None,
         conditions: list[Union[Condition, dict]] = None,
+        citations: list[Union[Citation, dict]] = None,
     ):
         super().__init__()
         self.key = key
         self.description = description
         self.conditions = conditions if conditions else []
+        self.citations = citations if citations else []
         validate_required(self)
 
     @property
