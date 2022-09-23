483bf9d9be93143d527e6cb1be39b97482558f0f
Reid Mello <30907815+rjmello@users.noreply.github.com>
2022-09-23 07:47:38 -0400

    Create paginators module

--- src/cript/api.py
+++ src/cript/api.py
@@ -37,6 +37,7 @@ from cript.exceptions import (
     DuplicateNodeError,
     FileSizeLimitError,
 )
+from cript.paginators import SearchPaginator
 
 
 logger = getLogger(__name__)
@@ -791,47 +792,3 @@ class API:
             if hasattr(instance, "url") and url == instance.url:
                 return instance
         return None
-
-
-class SearchPaginator:
-    """Paginate search JSON response content."""
-
-    def __init__(self, session, content, payload):
-        self._session = session
-        self.payload = payload
-        self.current = content
-        self.count = self.current["count"]
-
-    def __repr__(self):
-        return json.dumps(self.current, indent=4)
-
-    def __str__(self):
-        return json.dumps(self.current, indent=4)
-
-    @property
-    def current(self):
-        return self._current
-
-    @current.setter
-    def current(self, value):
-        self._current = json.loads(value)
-
-    @property
-    def next(self):
-        """Flip to the next page."""
-        next_url = self.current["next"]
-        if next_url:
-            response = self._session.post(url=next_url, data=self.payload)
-            self.current = response.content
-        else:
-            raise AttributeError("You've reached the end of the query.")
-
-    @property
-    def previous(self):
-        """Flip to the previous page."""
-        previous_url = self.current["previous"]
-        if previous_url:
-            response = self._session.post(url=previous_url, data=self.payload)
-            self.current = response.content
-        else:
-            raise AttributeError("You've reached the beginning of the query.")
