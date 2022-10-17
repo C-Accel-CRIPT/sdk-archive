44cfd9fcf2542ff17464252415125a0000b35c04
Reid Mello <30907815+rjmello@users.noreply.github.com>
2022-10-16 21:54:09 -0400

    Move Paginator class to data_model dir

--- src/cript/data_model/paginator.py
+++ src/cript/data_model/paginator.py
@@ -1,41 +1,48 @@
 import copy
+from typing import Union
 from urllib.parse import urlparse
 from urllib.parse import urlunparse
 from urllib.parse import parse_qs
 from urllib.parse import urlencode
 
-from cript.exceptions import InvalidPage
-from cript.exceptions import APISearchError
-from cript.utils import display_errors
+from beartype import beartype
+
+from cript.cache import get_cached_api_session
+from cript.cache import get_cached_node
+from cript.data_model.utils import get_data_model_class
+from cript.data_model.utils import create_node
+from cript.data_model.exceptions import InvalidPage
+from cript.api.exceptions import APIError
 
 
 class Paginator:
-    """Paginator for object lists and raw JSON."""
+    """
+    Paginator for object lists and raw JSON.
+    """
 
+    @beartype
     def __init__(
         self,
-        url,
-        api=None,
-        obj_class=None,
-        payload=None,
-        limit=None,
-        offset=None,
-        max_level=0,
+        url: str,
+        node_name: str,
+        payload: Union[str, None] = None,
+        limit: Union[int, None] = None,
+        offset: Union[int, None] = None,
+        max_level: int = 0,
     ):
         """
         Initializes a Paginator object.
 
         :param url: Query URL
-        :param api: API object instance
-        :param obj_class: Relevant class for object generation
+        :param node_class: Relevant class for object generation
+        :param payload: POST request payload
         :param limit: The max number of items per page.
         :param offset: The starting position of the paginator.
         :param max_level: Max depth to recursively generate nested nodes.
-        :param payload: POST request payload
         """
         self.url = url
-        self.api = api
-        self.obj_class = obj_class
+        self.api = get_cached_api_session(url)
+        self.node_class = get_data_model_class(node_name)
         self.limit = limit
         self.offset = offset
         self.max_level = max_level
@@ -44,7 +51,9 @@ class Paginator:
         self._count = None
 
     def json(self):
-        """Get the raw JSON."""
+        """
+        Get the raw JSON.
+        """
         if self.api is None:
             raise AttributeError("The 'api' attribute must be defined.")
 
@@ -66,42 +75,42 @@ class Paginator:
 
         # Get JSON response
         if self.payload:
-            response = self.api.session.post(self.url, data=self.payload)
+            response = self.api.post(self.url, data=self.payload, valid_codes=[200])
         else:
-            response = self.api.session.get(self.url)
+            response = self.api.get(self.url)
 
-        response_json = response.json()
-        if "results" not in response_json:
-            raise APISearchError(display_errors(response.content))
+        if "results" not in response:
+            raise APIError(response)
 
-        self._raw = response_json
+        self._raw = response
         return self._raw["results"]
 
     def objects(self):
-        """Use the current raw JSON to generate a list of objects."""
+        """
+        Use the current raw JSON to generate a list of objects.
+        """
         if self._raw is None:
             self.json()
-
-        if self.obj_class is None:
-            raise AttributeError("The 'obj_class' attribute must be defined.")
 
         objs_json = copy.deepcopy(self._raw["results"])
         obj_list = []
         for obj_json in objs_json:
             # Use the local object if it's in memory
             # Otherwise, create a new object
-            local_obj = self.api._get_local_node(obj_json["url"])
+            local_obj = get_cached_node(obj_json["url"])
             if local_obj:
                 obj = local_obj
             else:
-                obj = self.api._create_node(self.obj_class, obj_json)
-                self.api._generate_nodes(obj, max_level=self.max_level)
+                obj = create_node(self.node_class, obj_json)
+                obj._generate_nested_nodes(max_level=self.max_level)
             obj_list.append(obj)
 
         return obj_list
 
     def next_page(self):
-        """Flip to the next page."""
+        """
+        Flip to the next page.
+        """
         if self._raw is None:
             self.json()
 
@@ -114,7 +123,9 @@ class Paginator:
             raise InvalidPage("You're currently on the last page.")
 
     def previous_page(self):
-        """Flip to the previous page."""
+        """
+        Flip to the previous page.
+        """
         if self._raw is None:
             self.json()
 
@@ -127,7 +138,9 @@ class Paginator:
             raise InvalidPage("You're currently on the first page.")
 
     def count(self):
-        """Get the total number of objects."""
+        """
+        Get the total number of objects.
+        """
         if self._raw is None:
             self.json()
 
