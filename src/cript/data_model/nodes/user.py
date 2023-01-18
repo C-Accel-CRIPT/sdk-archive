399b595ce72a68c0975a515e2a9c7892b51d5146
Eric Muckley <ericmuckley@gmail.com>
2023-01-18 14:01:01 -0800

    added docstrings to User node

--- src/cript/data_model/nodes/user.py
+++ src/cript/data_model/nodes/user.py
@@ -8,11 +8,31 @@ logger = getLogger(__name__)
 
 
 class User(BaseNode):
-    """
-    Object representing a CRIPT user.
+    """The <a href="../user" target="_blank">`User`</a> object represents a single CRIPT user.
 
-    Note: A user cannot be created or modified using the SDK.
-          This object is for read-only purposes only.
+    !!! warning "Modifying CRIPT users"
+        CRIPT `User` objects cannot be created or modified using the Python SDK.
+        The `User` object documented here is for informational purposes only.
+
+    Args:
+        username (str, optional): Username displayed on the CRIPT interface
+        email (str, optional): Email of the user
+        orcid_id (str, optional): ORCID ID of the user
+        groups (_type_, optional): `Group` objects that the user is a member of
+        public (bool, optional): Whether the user is publicly viewable
+     
+    ``` json title="Example of a user in JSON format"
+    {
+        "url": "https://criptapp.org/api/user/8de042c7-ba6e-4e4e-9f6e-0fdeb73e4595/",
+        "uid": "8de042c7-ba6e-4e4e-9f6e-0fdeb73e4595",
+        "username": "esm",
+        "email": "esm@email.com",
+        "orcid_id": "0000-0001-7114-5424",
+        "public": true,
+        "created_at": "2022-04-18T17:35:30.019800Z",
+        "updated_at": "2023-01-17T18:34:42.186527Z"
+    },
+    ```
     """
 
     node_name = "User"
