e7b5155d3cce2b7043e56863fbc7204f6a96b11b
Eric Muckley <ericmuckley@gmail.com>
2023-01-18 14:11:56 -0800

    docstrings on Software node

--- src/cript/data_model/nodes/software.py
+++ src/cript/data_model/nodes/software.py
@@ -10,9 +10,37 @@ logger = getLogger(__name__)
 
 
 class Software(BaseNode):
-    """
-    Object representing a computation tool, code, programing language,
-    or software package.
+    """The <a href="../software" target="_blank">`Software`</a> object
+    represents a computational tool, a set of computer code,
+    a programming language, or a software package.
+
+    Args:
+        group (Union[Group, str]): `Group` object that manages the software object
+        name (str): Software name
+        version (str): Software version
+        source (Union[str, None], optional): Software source
+        notes (Union[str, None], optional): Software notes
+        public (bool, optional): Whether the `Software` object is publicly viewable
+
+    !!! success "Software methods"
+        Since the `Software` object inherits from the <a href="../base_node" target="_blank">`BaseNode`</a> object,
+        all the <a href="../base_node" target="_blank">`BaseNode`</a> object methods can be used to manipulate a `Software` object.
+        These include `get()`, `create()`, `delete()`, `save()`, `search()`, `update()`, and `refresh()` methods.
+        See the <a href="../base_node" target="_blank">`BaseNode`</a> documentation to learn more about these methods.
+    
+    ``` py title="Example"
+    # get an existing group
+    my_group = Group.get(name="My group")
+
+    # create the software
+    my_software = Software.create(
+        group=my_group,
+        name="Python",
+        version="3.9",
+        source="Anaconda distribution",
+        notes="Used for processing NMR data",
+    )
+    ```
     """
 
     node_name = "Software"
